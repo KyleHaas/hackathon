c71c04584004eb91576ee7143284b599f7f44878
KyleHaas <Kyle@172.17.37.246>
2017-02-23 16:57:13 -0600

    Abstracting config classes

--- src/main/java/com/cbsh/aws/config/KinesisConsumerConfig.java
+++ src/main/java/com/cbsh/aws/config/KinesisConsumerConfig.java
@@ -1,7 +1,5 @@
 package com.cbsh.aws.config;
 
-import java.util.List;
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -13,37 +11,6 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @ConfigurationProperties("camel.routes.kinesis-consumer")
-public class KinesisConsumerConfig {
-
-    /** Source of data to process. **/
-    private String from;
-    /**
-     * Whether or not the 'to' destinations should be processed in parallel or not.
-     * if set to false, 'to' endpoints will be processed in the order provided in
-     * the yml file.
-     **/
-    private Boolean parallel;
-    /** List of endpoints to send the 'transformed' data to. **/
-    private List<String> to;
-
-    public Boolean getParallel() {
-        return parallel;
-    }
-    public void setParallel(Boolean parallel) {
-        this.parallel = parallel;
-    }
-    public String getFrom() {
-        return from;
-    }
-    public void setFrom(String from) {
-        this.from = from;
-    }
-    public List<String> getTo() {
-        return to;
-    }
-    public void setTo(List<String> to) {
-        this.to = to;
-    }
-
+public class KinesisConsumerConfig extends AbstractEnpointConfiguration{
 
 }
--- src/main/java/com/cbsh/aws/config/S3ToKinesisEndpointConfiguration.java
+++ src/main/java/com/cbsh/aws/config/S3ToKinesisEndpointConfiguration.java
@@ -1,7 +1,5 @@
 package com.cbsh.aws.config;
 
-import java.util.List;
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -13,37 +11,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @ConfigurationProperties("camel.routes.s3-to-kinesis")
-public class S3ToKinesisEndpointConfiguration {
-	
-	/** Source of data to process. **/
-	private String from;
-	/** 
-	 * Whether or not the 'to' destinations should be processed in parallel or not.
-	 * if set to false, 'to' endpoints will be processed in the order provided in 
-	 * the yml file.
-	 **/
-	private Boolean parallel;
-	/** List of endpoints to send the 'transformed' data to. **/
-	private List<String> to;
-	
-	public Boolean getParallel() {
-		return parallel;
-	}
-	public void setParallel(Boolean parallel) {
-		this.parallel = parallel;
-	}
-	public String getFrom() {
-		return from;
-	}
-	public void setFrom(String from) {
-		this.from = from;
-	}
-	public List<String> getTo() {
-		return to;
-	}
-	public void setTo(List<String> to) {
-		this.to = to;
-	}
+public class S3ToKinesisEndpointConfiguration extends AbstractEnpointConfiguration{
 	
 	
 }
